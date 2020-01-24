8eb3c66502125bbb49cf4d670606ad130360190a
CUB3D <callumthom11@gmail.com>
2020-01-24 21:46:21 +0000

    Fixed scrolling notes

--- app/src/main/java/pw/cub3d/cub3_notes/ui/home/HomeFragment.kt
+++ app/src/main/java/pw/cub3d/cub3_notes/ui/home/HomeFragment.kt
@@ -31,6 +31,8 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        home_nestedScroll.isNestedScrollingEnabled = false
+
         homeViewModel.pinnedNotes.observe(this, Observer {
             if(it.isEmpty()) {
                 home_pinnedNotes.visibility = View.GONE
