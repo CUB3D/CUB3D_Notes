65b4ce9e4ecdc6e5e20cc1f2838e21dbe3c4f8f2
CUB3D <callumthom11@gmail.com>
2020-03-11 19:01:27 +0000

    Links in editor now clickable

--- app/src/main/java/pw/cub3d/cub3_notes/sync/OpenTasksSyncManager.kt
+++ app/src/main/java/pw/cub3d/cub3_notes/sync/OpenTasksSyncManager.kt
@@ -6,7 +6,8 @@ import android.net.Uri
 import javax.inject.Inject
 import javax.inject.Singleton
 
-
+//https://github.com/dmfs/opentasks/blob/88a57514b90bc3bdfd81b28e7af24ed52078545e/opentasks-contract/src/main/java/org/dmfs/tasks/contract/TaskContract.java
+//https://github.com/dmfs/opentasks/tree/master/opentasks-provider/src/main/java/org/dmfs/provider/tasks
 @Singleton
 class OpenTasksSyncManager @Inject constructor(
     private val context: Context
